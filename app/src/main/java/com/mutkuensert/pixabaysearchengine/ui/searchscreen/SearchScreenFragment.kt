db0af9f9a3bdde84fe240d6f12f0e3efbf47914d
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-07-21 23:55:23 +0300

    I'm committing this because: I was trying to implement a spinner with a custom design(custom_spinner_item) and with shadow. Then, I realised that implementing shadow with xml code includes so much boilerplate code. Then I saw someone suggested wrapping spinner with card view because it has elevation feature(shadow). Actually it's unnecessary to use custom spinner design when you wrap the spinner with card view. But, I tried it anyway. Then I saw that when I use custom_spinner_item in ArrayAdapter implementation, cardview elevation doesn't work without app:cardUseCompatPadding="true" feature. More details are in source code. I'll try to simplify the codes in next commit.

--- app/src/main/java/com/mutkuensert/pixabaysearchengine/ui/searchscreen/SearchScreenFragment.kt
+++ app/src/main/java/com/mutkuensert/pixabaysearchengine/ui/searchscreen/SearchScreenFragment.kt
@@ -1,15 +1,22 @@
 package com.mutkuensert.pixabaysearchengine.ui.searchscreen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.mutkuensert.pixabaysearchengine.R
 import com.mutkuensert.pixabaysearchengine.databinding.FragmentSearchScreenBinding
 
+const val TAG = "SearchScreenFragment"
 class SearchScreenFragment : Fragment() {
     private var _binding: FragmentSearchScreenBinding? = null
     private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,8 +29,65 @@ class SearchScreenFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setSpinners()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setSpinners(){
+
+        val imageAndVideo = arrayOf("image","video")
+
+        ArrayAdapter(context!!, R.layout.custom_spinner_item, imageAndVideo).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.imageOrVideoSpinner.adapter = it
+        }
+
+        binding.imageOrVideoSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0!!.getItemAtPosition(p2).also {
+                    setImageOrVideoTypeSpinnerIngredients(it as String)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //Will be implemented
+            }
+
+        }
+
+    }
+
+    private fun setImageOrVideoTypeSpinnerIngredients(type: String){
+        val imageTypes = arrayOf("all","photo","illustration","vector") //all is default in api
+        val videoTypes = arrayOf("all","film","animation") //all is default in api
+        var types = arrayOf<String>()
+
+        if(type == "image"){
+            types = imageTypes
+        } else if(type == "video"){
+            types = videoTypes
+        }
+
+        ArrayAdapter(context!!, R.layout.custom_spinner_item, types).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.imageOrVideoTypeSpinner.adapter = it
+        }
+
+        binding.imageOrVideoTypeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                //Will be implemented
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //Will be implemented
+            }
+
+        }
     }
 }
